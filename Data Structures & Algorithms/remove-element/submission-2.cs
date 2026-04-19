c0280e6480bc106f2e0ecc55918e8f5e7372680d
Greimil <84160882+Greimil@users.noreply.github.com>
2026-04-19 17:29:02 -0400

    Add: remove-element - submission-2

---
+++ Data Structures & Algorithms/remove-element/submission-2.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        int w = 0;
+
+        for (int r = 0; r < nums.Length; r++) {
+            if (nums[r] != val) {
+                nums[w] = nums[r];
+                w++;
+            }
+        }
+    
+        return w;
+    
+    }
+
+
+}
